e3c0125e3e2375d5ad04b2577a5c50082d26cb49
Valmir França <vfranca3@gmail.com>
2025-08-24 18:21:32 -0300

    Revisando estrutura do plugin

--- mtcli_rsi/rsi.py
+++ mtcli_rsi/rsi.py
@@ -2,74 +2,87 @@ import click
 import MetaTrader5 as mt5
 import pandas as pd
 import sqlite3
-import logging
-from datetime import datetime
+from mtcli.conecta import conectar, shutdown
+from mtcli.logger import setup_logger
 
-# Configurar logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler('rsi.log')
-file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logger = setup_logger("rsi")
 
-# console_handler = logging.StreamHandler()
-# console_handler.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
-
-logger.addHandler(file_handler)
-# logger.addHandler(console_handler)
 
 @click.command()
-@click.option('--symbol', default='WINQ25', help='Símbolo do ativo (ex: WINQ25)')
-@click.option('--timeframe', default='M5', help='Timeframe (ex: M1, M5, H1, D1)')
-@click.option('--period', default=9, help='Período do RSI')
-@click.option('--bars', default=20, help='Número de candles para análise')
-@click.option('--db', default='rsi.db', help='Arquivo SQLite (ex: dados.db)')
-def rsi(symbol, timeframe, period, bars, db):
-    if not mt5.initialize():
-        logger.error(f"Erro ao conectar MT5: {mt5.last_error()}")
-        return
+@click.version_option(package_name="mtcli-rsi")
+@click.option(
+    "--symbol", "-s", default="WIN$N", help="Símbolo do ativo (default WIN$N)."
+)
+@click.option("--period", "-p", default="M5", help="Timeframe (default M5)")
+@click.option("--periodo", "-pe", default=9, help="Período do RSI (default 9).")
+@click.option(
+    "--bars", type=int, default=20, help="Número de candles para análise (default 20)."
+)
+@click.option("--db", default="rsi.db", help="Arquivo SQLite (default rsi.db).")
+def rsi(symbol, period, periodo, bars, db):
+    """Calcula o RSI do ativo symbol."""
+    conecta()
 
     tf_map = {
-        'M1': mt5.TIMEFRAME_M1,
-        'M2': mt5.TIMEFRAME_M2,
-        'M5': mt5.TIMEFRAME_M5,
-'M15': mt5.TIMEFRAME_M15,
-        'H1': mt5.TIMEFRAME_H1,
-        'D1': mt5.TIMEFRAME_D1
+        "M1": mt5.TIMEFRAME_M1,
+        "M2": mt5.TIMEFRAME_M2,
+        "M3": mt5.TIMEFRAME_M3,
+        "M4": mt5.TIMEFRAME_M4,
+        "M5": mt5.TIMEFRAME_M5,
+        "M6": mt5.TIMEFRAME_M6,
+        "M10": mt5.TIMEFRAME_M10,
+        "M12": mt5.TIMEFRAME_M12,
+        "M15": mt5.TIMEFRAME_M15,
+        "M20": mt5.TIMEFRAME_M20,
+        "M30": mt5.TIMEFRAME_M30,
+        "H1": mt5.TIMEFRAME_H1,
+        "H2": mt5.TIMEFRAME_H2,
+        "H3": mt5.TIMEFRAME_H3,
+        "H4": mt5.TIMEFRAME_H4,
+        "H6": mt5.TIMEFRAME_H6,
+        "H8": mt5.TIMEFRAME_H8,
+        "H12": mt5.TIMEFRAME_H12,
+        "D1": mt5.TIMEFRAME_D1,
+        "W1": mt5.TIMEFRAME_W1,
+        "MN1": mt5.TIMEFRAME_MN1,
     }
 
-    if timeframe not in tf_map:
-        logger.error("Timeframe inválido.")
+    if period.upper() not in tf_map:
+        logger.error(f"Timeframe {period} inválido.")
         return
 
-    rates = mt5.copy_rates_from_pos(symbol, tf_map[timeframe], 0, bars + period)
-    mt5.shutdown()
+    rates = mt5.copy_rates_from_pos(symbol, tf_map[period], 0, bars + periodo)
+    shutdown()
 
     if rates is None or len(rates) == 0:
-        logger.error("Erro ao obter dados.")
+        logger.error(f"Erro ao obter dados do {symbol}.")
         return
 
     df = pd.DataFrame(rates)
-    df['close'] = df['close'].astype(float)
-    df['delta'] = df['close'].diff()
-    gain = df['delta'].where(df['delta'] > 0, 0.0)
-    loss = -df['delta'].where(df['delta'] < 0, 0.0)
+    df["close"] = df["close"].astype(float)
+    df["delta"] = df["close"].diff()
+    gain = df["delta"].where(df["delta"] > 0, 0.0)
+    loss = -df["delta"].where(df["delta"] < 0, 0.0)
 
-    avg_gain = gain.rolling(window=period).mean()
-    avg_loss = loss.rolling(window=period).mean()
+    avg_gain = gain.rolling(window=periodo).mean()
+    avg_loss = loss.rolling(window=periodo).mean()
     rs = avg_gain / avg_loss
-    df['RSI'] = 100 - (100 / (1 + rs))
-    df['RSI'] = df['RSI'].round(0)
+    df["RSI"] = 100 - (100 / (1 + rs))
+    df["RSI"] = df["RSI"].round(0)
 
-    df_result = df[['RSI', 'close', 'time']].dropna()
-    df_result['time'] = pd.to_datetime(df_result['time'], unit='s')
+    df_result = df[["RSI", "close", "time"]].dropna()
+    df_result["time"] = pd.to_datetime(df_result["time"], unit="s")
     click.echo(df_result.tail(5).to_string(index=False))
 
     conn = sqlite3.connect(db)
-    df_result.to_sql('rsi', conn, if_exists='append', index=False)
+    df_result.to_sql("rsi", conn, if_exists="append", index=False)
     conn.close()
 
-    logger.info(f'Dados RSI salvos em {db} (tabela rsi) - símbolo: {symbol}, timeframe: {timeframe}, período: {period}')
+    logger.info(
+        f"Dados RSI salvos em {db} (tabela rsi) - símbolo: {symbol}, timeframe: {period}, período: {periodo}"
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     rsi()
